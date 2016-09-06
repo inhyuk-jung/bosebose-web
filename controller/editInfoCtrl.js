368fbac5373019fdeb2321fd59f71cf622a196e8
Jung In Huyk <jihsss@naver.com>
2016-09-06 14:21:41 +0900

    정보 수정란 서버와 연결

--- controller/editInfoCtrl.js
+++ controller/editInfoCtrl.js
@@ -4,51 +4,37 @@ $.ajax({
   type: 'POST',
   contentType: false,
   success: function (data) {
-    //if (data.data.success) {
-    //  console.log(data);
-    //} else {
-    //  console.log("실패");
-    //  console.log(data);
-    //}
+    if (data.data.success) {
+      var storeData = data.data.store;
+      var storeDataDOM = document.forms["myInfoForm"];
+      storeDataDOM.elements["name"].value = storeData.name;
+      storeDataDOM.elements["address"].value = storeData.address;
+      storeDataDOM.elements["introduction"].value = storeData.introduction;
+      storeDataDOM.elements["business_hour"].value = storeData.business_hour;
+      storeDataDOM.elements["holiday"].value = storeData.holiday;
+      storeDataDOM.elements["phone"].value = storeData.phone;
+      //storeDataDOM.elements["s_image"].value = storeData.image;
+      $('#hit').value = storeData.hit;
+      $('#item_count').value = storeData.item_count;
+      $('#like_count').value = storeData.like_count;
+      console.log(data);
+    } else {
+      console.log("실패");
+      console.log(data);
+    }
   },
   error: function(err) {
     console.log(err);
   }
 });
 
-
-
-$('#submitSignUp').click(function(e) {
-  var signUpForm = document.forms["signUpForm"];
-  var signUpFormData = new FormData(signUpForm);
-
-  $.ajax({
-    url: 'http://52.78.112.226:8888/Register',
-    data: signUpFormData,
-    processData: false,
-    type: 'POST',
-    contentType: false,
-    success: function (data) {
-      console.log(data);
-      // var url = "login.html";
-      // $(location).attr('href',url);
-    },
-    error: function(err) {
-      console.log(err);
-    }
-  });
-  // var url = "login.html";
-  // $(location).attr('href',url);
-});
-
-$('#submitStoreReg').click(function(e) {
-  //회원가입도야 하게 만들어져있는데 수정필요
-  var storeRegForm = document.forms["storeRegForm"];
-  var storeRegFormData = new FormData(storeRegForm);
+$('#submitEditMyInfo').click(function(e) {
+  var myInfoForm = document.forms["myInfoForm"];
+  var myInfoFormData = new FormData(myInfoForm);
 
   $.ajax({
     url: 'http://52.78.112.226:8888/RegisterStore',
-    data: storeRegFormData,
+    data: myInfoFormData,
     processData: false,
     type: 'POST',
     contentType: false,
@@ -64,3 +50,4 @@ $('#submitStoreReg').click(function(e) {
   // var url = "login.html";
   // $(location).attr('href',url);
 });
+
